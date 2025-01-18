b25315c2902d06720c51f5add7c63375cafa91e9
Sameer Diwakar <diwakarsameer27@gmail.com>
2025-01-18 12:01:23 +0530

    Non-Auth Routes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,61 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode,sign,verify } from 'hono/jwt';
 
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string
+		JWT_SECRET: string
 	}
 }>();
 
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/user/signup',async (c) => {
+	const body = await c.req.json()
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env?.DATABASE_URL,
 	}).$extends(withAccelerate())
-	return c.text('signup route')
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		})
+		const jwt = await sign({
+			id: user.id
+		},c.env.JWT_SECRET);
+		return c.text(jwt)
+	} catch (error) {
+		c.status(411);
+		return c.text('Inavlid email or password')
+	}
 })
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route')
+app.post('/api/v1/user/signin',async (c) => {
+	const body = await c.req.json()
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate())
+	try {
+		const user = await prisma.user.findFirst({
+			where: {
+				email: body.email,
+				password: body.password
+			}
+		})
+		if(!user) {
+			c.status(403); 
+			return c.text('Inavlid email or password')
+		}
+		const jwt = await sign({
+			id: user.id
+		},c.env.JWT_SECRET);
+		return c.text(jwt)
+	} catch (error) {
+		c.status(411);
+		return c.text('Inavlid Request')
+	}
 })
 
 app.get('/api/v1/blog/:id', (c) => {
