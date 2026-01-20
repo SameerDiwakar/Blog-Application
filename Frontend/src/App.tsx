83fc48d5aff7b5982370a6a150bcfa05c9901a5f
Sameer Diwakar <diwakarsameer27@gmail.com>
2026-01-20 19:29:47 +0530

    fix: add root route redirect

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Signup from './pages/Signup'
 import  Signin  from './pages/Signin'
 import  Blog  from './pages/Blog'
@@ -11,11 +11,20 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route
+            path="/"
+            element={
+              localStorage.getItem("token")
+                ? <Navigate to="/blogs" replace />
+                : <Navigate to="/signin" replace />
+            }
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </>
