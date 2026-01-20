fd584c7cb160122b5a1d92d5244934f6c9cc4dd3
Sameer Diwakar <diwakarsameer27@gmail.com>
2026-01-20 19:13:44 +0530

    Frontend Deployment Config

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "https://medium-project.diwakarsameer27.workers.dev"
+const backendUrl = import.meta.env.VITE_BACKEND_URL ?? "https://medium-project.diwakarsameer27.workers.dev";
+
+export const BACKEND_URL = backendUrl;
